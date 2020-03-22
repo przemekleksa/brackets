878bdea19a7322ad599c501b5f111e2263b07ab9
Przemyslaw Leksa <38156110+gnoosny@users.noreply.github.com>
2020-03-22 17:12:21 +0100

    :)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   str = str.split("");  
+   let result = [];
+   
+   for (let i = 0; i < str.length; i++) {
+     for(let j = 0; j < bracketsConfig.length; j++) { 
+
+       if(str[i] === bracketsConfig[j][0]) {
+         
+         result.push(bracketsConfig[j][0]);
+         if(str[i] === bracketsConfig[j][1] &&
+            str[i] === result[result.length-2]) {
+           result.pop();
+           result.pop();
+         }
+       } 
+       else if (str[i] == bracketsConfig[j][1]) {
+         if(result[result.length-1] != bracketsConfig[j][0]) return false;
+ 
+         result.pop();          
+       }
+     }        
+   }
+   
+   return result.length === 0;
 }
